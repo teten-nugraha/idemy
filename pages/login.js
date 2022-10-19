311f7f8b6d51666eb9762dbf4a6a30a784ce9c22
Teten Nugraha <teten.nugraha18@gmail.com>
2022-10-19 09:10:06 +0700

    feat: apply register page

---
+++ pages/login.js
@@ -0,0 +1,10 @@
+const Login = () => {
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary square">Login</h1>
+      <p>from NextJS</p>
+    </>
+  );
+};
+
+export default Login;
