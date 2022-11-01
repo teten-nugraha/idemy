c4979997d1e2a3593e2ef616de6bf387da53825b
Teten Nugraha <teten.nugraha18@gmail.com>
2022-11-01 14:46:35 +0700

    feat: setup logout feature

--- components/TopNav.js
+++ components/TopNav.js
@@ -3,12 +3,40 @@ import Link from "next/link";
 import {
   AppstoreOutlined,
   LoginOutlined,
-  UserAddOutlined
+  UserAddOutlined,
+  LogoutOutlined
 } from "@ant-design/icons";
+import { Context } from "../context";
+import {useContext, useEffect, useState} from "react";
+import axios from "axios";
+import {toast} from "react-toastify";
+import { useRouter } from "next/router";
 
 const { Item } = Menu;
 
 const TopNav = () => {
+
+    const [current, setCurrent] = useState("");
+    const { state, dispatch } = useContext(Context);
+
+    const router = useRouter();
+
+    useEffect(() => {
+        process.browser && setCurrent(window.location.pathname);
+    }, [process.browser && window.location.pathname]);
+
+    const logout = async () => {
+        dispatch({
+            type: "LOGOUT"
+        });
+        window.localStorage.removeItem("user");
+        window.localStorage.removeItem("jwt");
+        const { data } = await axios.get('/api/logout');
+        toast(data.message);
+        router.push('/login');
+    }
+
+
   return (
     <Menu mode="horizontal">
       <Item icon={<AppstoreOutlined />}>
@@ -28,6 +56,14 @@ const TopNav = () => {
           <a>Register</a>
         </Link>
       </Item>
+
+    <Item
+        onClick={logout}
+        icon={<LogoutOutlined />}
+        className="float-right"
+    >
+        Logout
+    </Item>
     </Menu>
   );
 };
